e96c38931a298f6b2c6cfe3280f994a823e198ee
geropl <gero.posmyk-leinemann@typefox.io>
2019-03-01 16:09:44 +0000

    Read cgroups

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 extern crate tokio;
 extern crate shiplift;
-// extern crate sysinfo;
+extern crate sysinfo;
+//extern crate cgroup_fs;
 
 use std::env;
 use tokio::prelude::Future;
 use shiplift::{ Docker };
-// use sysinfo::{ SystemExt, System };
+use shiplift::rep::{ ContainerDetails };
+use sysinfo::{ SystemExt, System };
 
+use cgroups_fs;
+// [0].HostConfig.CgroupParent
+
+// docker ps -> docker id -> container
 fn main() {
     let id = env::args()
         .nth(1)
@@ -15,9 +21,30 @@ fn main() {
     let docker = Docker::new();
     let f = docker.containers()
         .get(&id)
-        .top(Option::None)
-        .map(|top| println!("{:#?}", top))
+        .inspect()
+        .map(|details: ContainerDetails| {
+            println!("{:#?}", details);
+            details.host_config.cgroup_parent.expect("cgroup_parent not set")
+        })
+        .map(|cgroup_parent| {
+
+            let mut sys = System::new();
+            sys.refresh_processes();
+            for process in sys.get_process_list() {
+                let (_pid, p) = process;
+                println!("{}", p.name);
+                println!("{:?}", process);
+            }
+
+
+            println!("{:#?}", &cgroup_parent);
+            let name = cgroups_fs::CgroupName::new(cgroup_parent);
+            let group_memory = cgroups_fs::Cgroup::new(&name, "memory");
+            let mem_limit_in_bytes: u64 = group_memory.get_value("memory.limit_in_bytes").expect("Cannot read memory.limit_in_bytes");
+            println!("memory.limit_in_bytes: {}", mem_limit_in_bytes);
+        })
         .map_err(|e| eprintln!("Error: {}", e));
+
     tokio::run(f);
 
     // let mut sys = System::new();
